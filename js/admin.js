25997538ff949b9530ffe7f8f8709c009291eed8
= <=>
2024-02-08 07:59:39 +0700

    selesai alhamdulillah

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,15 @@
 const logo = document.getElementById('logo');
 const subMenu = document.getElementById('sub-menu-wrapper');
+const tutup = document.getElementById('tutup');
+const myAdmin = document.getElementById('myModal');
+const myBtn = document.getElementById('myBtn');
+
+myBtn.addEventListener('click', ()=>{
+    myAdmin.style.display = 'block';
+})
+tutup.addEventListener('click', ()=>{
+    myAdmin.style.display = 'none';
+})
 
 logo.addEventListener('click', ()=>{
     subMenu.classList.toggle('tampil');    
